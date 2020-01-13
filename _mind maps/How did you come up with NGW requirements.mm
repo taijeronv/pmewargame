128cb4fd1900dc5b9fafbfe7c028e23f61e55ccc
taijeronv <taijeronv@gmail.com>
2020-01-13 12:11:14 +0200

    200113-1 updates

---
+++ _mind maps/How did you come up with NGW requirements.mm
@@ -0,0 +1,79 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1578892290005" ID="ID_606414321" MODIFIED="1578892528146" TEXT="How did you come up with NGW requirements?">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1578892310689" ID="ID_941930490" MODIFIED="1578892409077" POSITION="right" TEXT="Technical Requirements">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1578892337932" ID="ID_1986673068" MODIFIED="1578892409079" TEXT="Playable in a web browser">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1578892345522" ID="ID_1802160193" MODIFIED="1578892409080" TEXT="OS agnostic">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1578892433715" ID="ID_287039738" MODIFIED="1578892440088" TEXT="Playable over the internet">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1578892319641" ID="ID_819560221" MODIFIED="1578892409082" POSITION="left" TEXT="Functional Requirements">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1578892359279" ID="ID_759164523" MODIFIED="1578892409084" TEXT="Easy to play">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1578892362960" ID="ID_436956993" MODIFIED="1578892409084" TEXT="Simple interface">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1578892370230" ID="ID_869775437" MODIFIED="1578892409086" TEXT="Flat learning curve">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1578892326743" ID="ID_1721804180" MODIFIED="1578892512158" POSITION="right" TEXT="TDG Requirements">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1578892381112" ID="ID_1080817842" MODIFIED="1578892409088" TEXT="Events">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1578892388137" ID="ID_24048929" MODIFIED="1578892409089" TEXT="Triggers">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1578892395341" ID="ID_25994978" MODIFIED="1578892409091" POSITION="left" TEXT="AI requirements">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1578892451592" ID="ID_355107017" MODIFIED="1578892456910" TEXT="Unit behaviors">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1578892459915" ID="ID_1664768635" MODIFIED="1578892464545" TEXT="Enemy actions">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1578892478412" ID="ID_624732473" MODIFIED="1578892482667" POSITION="right" TEXT="Introduction">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1578892485498" ID="ID_1883138572" MODIFIED="1578892497547" TEXT="What makes NGW &quot;next generation&quot;?">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1578892539582" ID="ID_617165143" MODIFIED="1578892543882" POSITION="left" TEXT="Game requirements">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1578892545669" ID="ID_225621686" MODIFIED="1578892552072" TEXT="AAR playback">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
